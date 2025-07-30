f9ab8c5b19d41b8028abd5801bb93004cf334b36
London Bielicke <bielickelondon@gmail.com>
2025-07-30 16:27:56 -0700

    template

--- tutorial/tutorial.py
+++ tutorial/tutorial.py
@@ -4,42 +4,21 @@ from planet import *
 PLanet tutorial starter code
 """
 
-interface = ExperimentVariable(
-    name = "interface",
-    options=["baseline", "VR", "AR"]
+
+x = ExperimentVariable(
+    name = "",
+    options=["", ""]
 )
 
-units = Units(12)
+participants = Units()
 
-interface_design = (
+design = (
     Design()
-    .within_subjects(interface)
-    .absolute_rank(interface, "baseline", 1)
+  
 )
 
-assignment = assign(units, interface_design)
+assignment = assign(participants, design)
 print(assignment)
 
-
-
-task = ExperimentVariable(
-    name = "task",
-    options=["run", "walk", "sprint"]
-)
-
-task_design = (
-    Design()
-    .within_subjects(task)
-    .counterbalance(task)
-    .limit_groups(len(task))
-)
-
-# assignment = assign(units, task_design)
-# print(assignment)
-
-
-design = nest(inner=task_design, outer=interface_design)
-assignment = assign(units, design)
-print(assignment)
 
 
